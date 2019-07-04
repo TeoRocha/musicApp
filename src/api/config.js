f15fdba6d99aeafefe2f67df35e70feef7f858a9
QuietSoft <quiet.soft19@gmail.com>
2019-07-04 00:10:26 -0500

    Dev: Project starts

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+	appname: 'musicapp',
+	apiKey: 'a3793353bf6f0e036397bc9fd2a773ac',
+	secret: 'c2fc574479e0def39f95223ee3850653',
+	registeredTo: 'teorocha'
+}
+
+// Application name	musicapp
+// API key	a3793353bf6f0e036397bc9fd2a773ac
+// Shared secret	c2fc574479e0def39f95223ee3850653
+// Registered to	teorocha
